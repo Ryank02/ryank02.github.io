3153e4f87856c467c3ee197e107af5000f3f9e0e
Ryank02 <Ryank02@users.noreply.github.com>
2019-11-17 22:28:00 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,7 @@ document.body.ontouchend = function() {
 
 // About page
 const xhr = new XMLHttpRequest();
-xhr.open("OPTIONS", "/");
+xhr.open("HEAD", "/");
 xhr.onloadend = function() {
     const bDate = new Date("08/19/2002"),
            date = new Date(this.getResponseHeader("date"));
