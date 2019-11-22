d02fa0a69d130a06c0edecbd3abce7151de048aa
Ryank02 <rkroon76@hotmail.nl>
2019-11-22 23:35:26 +0100

    Simplified the javascript

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,30 +1,24 @@
 // Disable scrolling when overlay is open
-const elements = document.querySelectorAll("header div.nav-icon, div.flex div.col div.img-container");
-for (let i = 0, l = elements.length; i < l; i++) {
-    elements[i].onblur = function () { document.body.classList.remove("noscroll"); }
-    elements[i].onfocus = function () { document.body.classList.add("noscroll"); }
+const elem = document.querySelectorAll("div.nav-icon, div.img-container");
+for (let i = 0, l = elem.length; i < l; i++) {
+    elem[i].onblur = function () { document.body.classList.remove("noscroll"); }
+    elem[i].onfocus = function () { document.body.classList.add("noscroll"); }
 }
 
 // Swipe to open nav
-let touchstart;
-document.body.ontouchstart = function () { touchstart = event.changedTouches[0].clientX; }
-document.body.ontouchend = function () {
-    touchstart <= event.changedTouches[0].clientX - window.outerWidth / 6 && elements[0].blur();
-    touchstart >= event.changedTouches[0].clientX + window.outerWidth / 6 && elements[0].focus();
+let touch;
+document.body.ontouchstart = function (e) { touch = e.changedTouches[0].clientX; }
+document.body.ontouchend = function (e) {
+    touch <= e.changedTouches[0].clientX - window.outerWidth / 6 && elem[0].blur();
+    touch >= e.changedTouches[0].clientX + window.outerWidth / 6 && elem[0].focus();
 }
 
 // About page
-const xhr = new XMLHttpRequest();
-xhr.open("HEAD", "/");
-xhr.onload = function () {
-    const bDate = new Date("8/19/2002"),
-           date = new Date(xhr.getResponseHeader("date"));
-    document.querySelector("#age").innerHTML = ~~((date - bDate) / 31536e6);
-}
-xhr.send();
+const bDate = new Date("8/19/2002");
+document.getElementById("age").innerHTML = ~~((new Date() - bDate) / 31536e6);
 
 // Contact page
-document.querySelector("#redirect").value = window.location.origin + "/#contact";
-document.querySelector("#subjectInput").onchange = function () {
-    document.querySelector("#subject").value = this.value;
+document.getElementById("redirect").value = window.location.origin + "/#contact";
+document.getElementById("subjectInput").onchange = function () {
+    document.getElementById("subject").value = this.value;
 }
