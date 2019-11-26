c0837e2dadc8b8653077cc7632ee5eb0caa61417
Ryank02 <Ryank02@users.noreply.github.com>
2019-11-26 20:47:40 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,38 +1,33 @@
-// Variables
-const x = {
-    date: new Date("8/19/2002") - new Date(),
-    nodes: document.querySelectorAll("div.nav-icon, div.img-container"),
-    touch: null,
-    xhr: new XMLHttpRequest(),
-    xhrData: null
-};
-
 // Disable scrolling when overlay is open
-for (let i = x.nodes.length; i--;) {
-    x.nodes[i].onblur = function () { document.body.classList.remove("noscroll"); }
-    x.nodes[i].onfocus = function () { document.body.classList.add("noscroll"); }
+const elem = document.querySelectorAll("div.nav-icon, div.img-container");
+for (let i = elem.length; i--;) {
+    elem[i].onblur = function () { document.body.classList.remove("noscroll"); }
+    elem[i].onfocus = function () { document.body.classList.add("noscroll"); }
 }
 
 // Swipe to open nav
-document.body.ontouchstart = function (e) { x.touch = e.changedTouches[0].clientX; }
+let touch;
+document.body.ontouchstart = function (e) { touch = e.changedTouches[0].clientX; }
 document.body.ontouchend = function (e) {
-    x.touch <= e.changedTouches[0].clientX - window.outerWidth / 6 && x.nodes[0].blur();
-    x.touch >= e.changedTouches[0].clientX + window.outerWidth / 6 && x.nodes[0].focus();
+    touch <= e.changedTouches[0].clientX - window.outerWidth / 6 && elem[0].blur();
+    touch >= e.changedTouches[0].clientX + window.outerWidth / 6 && elem[0].focus();
 }
 
 // About page
-document.getElementById("age").innerHTML = ~~(-x.date / 31536e6);
+const date = new Date("8/19/2002") - new Date();
+document.getElementById("age").innerHTML = ~~(-date / 31536e6);
 
 // Contact page
 document.forms[0].onsubmit = function (e) {
     e.preventDefault();
-    x.xhrData = new FormData(this);
-    x.xhrData.append("*subject", this.elements.subject.value);
-    x.xhr.open(this.method, this.action);
-    x.xhr.onload = function () {
-        this.status === 200
+    const data = new FormData(this);
+    data.append("*subject", this.elements.subject.value);
+    const xhr = new XMLHttpRequest;
+    xhr.open(this.method, this.action);
+    xhr.onload = function () {
+        xhr.status === 200
             ? alert("Het bericht is verzonden!") && e.target.reset()
             : alert("Er is iets misgegaan, probeer het later aub opnieuw.");
     }
-    x.xhr.send(x.xhrData);
+    xhr.send(data);
 }
