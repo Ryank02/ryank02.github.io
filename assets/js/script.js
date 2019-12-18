a0e51be72119df7530143a49d2abf11137838a20
Ryank02 <Ryank02@users.noreply.github.com>
2019-12-18 20:45:39 +0000

    Deployed using Blazing fast GitHub Pages deploy

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,6 +6,20 @@ for (let i = elem.length; i--;) {
     elem[i].onfocus = scrollToggle;
 }
 
+// Open mobile nav by swiping
+const nav = document.getElementsByTagName("nav")[0];
+const swipe = document.getElementsByClassName("swipe")[0];
+swipe.ontouchstart = function (e) {
+    nav.style.cssText = "right: -" + e.changedTouches[0].pageX + "px; transition: none";
+};
+swipe.ontouchmove = function (e) {
+    nav.style.right = "-" + e.changedTouches[0].pageX + "px";
+    e.changedTouches[0].pageX <= nav.clientWidth / 3
+        ? elem[0].focus()
+        : elem[0].blur();
+}
+swipe.ontouchend = function () { nav.style.cssText = ""; }
+
 // About page
 const date = new Date("8/19/2002") - new Date();
 document.getElementById("age").innerHTML = ~~(-date / 31536e6);
