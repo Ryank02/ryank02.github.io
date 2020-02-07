2001f2a7ad66cc7afdb2300d4a612dc71a05492a
Ryank02 <rkroon76@hotmail.nl>
2020-02-07 11:35:53 +0100

    Make a fade animation with javascript

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,10 +1,25 @@
 // Disable scrolling when overlay is open
-const elem = document.querySelectorAll("header > a, div.img-container");
+const elems = document.querySelectorAll("header > a, div.img-container");
 function scrollToggle(e) { document.body.style.overflow = e.type === "blur" ? "" : "hidden"; }
-for (let i = elem.length; i--;) {
-    elem[i].onblur = scrollToggle;
-    elem[i].onfocus = scrollToggle;
+for (let i = elems.length; i--;) {
+    elems[i].onblur = scrollToggle;
+    elems[i].onfocus = scrollToggle;
 }
+
+// Fade animation when switching page
+const navLinks = document.querySelectorAll("nav a");
+const main = document.getElementsByTagName("main")[0];
+function switchPage(e) {
+    if (e.target.href !== window.location.href) {
+        e.preventDefault();
+        main.style.opacity = 0;
+        setTimeout(function() {
+            window.location = e.target.href;
+            main.style.cssText = "";
+        }, 300);
+    }
+}
+for (let i = navLinks.length; i--;) { navLinks[i].onclick = switchPage; }
 
 // About page
 const date = new Date("8/19/2002") - new Date();
